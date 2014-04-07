2d37fa72e91457a13e74c3a646b0757ce28e9e94
Angela Zhang <angelafzhang@gmail.com>
2014-04-06 21:35:59 -0400

    Initial commit. Empty.

---
+++ Drink Mixer/Drink Mixer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Drink Mixer
+//
+//  Created by Angela Zhang on 4/6/14.
+//  Copyright (c) 2014 Angela Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Drink Mixer/Drink MixerTests/Drink_MixerTests.m
@@ -0,0 +1,34 @@
+//
+//  Drink_MixerTests.m
+//  Drink MixerTests
+//
+//  Created by Angela Zhang on 4/6/14.
+//  Copyright (c) 2014 Angela Zhang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Drink_MixerTests : XCTestCase
+
+@end
+
+@implementation Drink_MixerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
