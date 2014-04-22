6d81c7867022f5b6b24effa63412c449f6a16d13
Angela Zhang <angelafzhang@gmail.com>
2014-04-22 16:48:20 -0400

    Started fb login.

---
+++ Drink Mixer/Drink Mixer/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  Drink Mixer
+//
+//  Created by Angela Zhang on 4/22/14.
+//  Copyright (c) 2014 Angela Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
