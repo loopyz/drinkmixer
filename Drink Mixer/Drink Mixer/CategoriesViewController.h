9d9b7bc072834a2b31f519039effaea1b879d4b0
Angela Zhang <angelafzhang@gmail.com>
2014-05-04 02:54:01 -0400

    Implemented CategoriesViewController as a UICollectionView delegate.

--- Drink Mixer/Drink Mixer/CategoriesViewController.h
+++ Drink Mixer/Drink Mixer/CategoriesViewController.h
@@ -7,15 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Firebase/Firebase.h>
 
-@interface CategoriesViewController : UIViewController
+#define firebaseURL @"https://something.firebaseio.com/" //TODO: change this to real thing!
+
+@interface CategoriesViewController : UIViewController <UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+{
+    UICollectionView *_collectionView;
+}
 
 @property (strong, nonatomic) IBOutlet UIButton *cocktailsButton;
 @property (strong, nonatomic) IBOutlet UIButton *smoothiesButton;
 @property (strong, nonatomic) IBOutlet UIButton *proteinButton;
 @property (strong, nonatomic) IBOutlet UIButton *otherButton;
 
-
+@property (nonatomic, strong) Firebase *firebase;
+@property (nonatomic, strong) NSDictionary *myDrinks;
+@property (nonatomic, strong) NSArray *drinkKeys;
 
 
 @end
