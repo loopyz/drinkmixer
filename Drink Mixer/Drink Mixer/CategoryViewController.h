638c227309bd0049708eb13fe7b19df7f708988a
Lucy Guo <lguo@andrew.cmu.edu>
2014-05-05 22:25:16 -0400

    WEEEE CATEGORIES

---
+++ Drink Mixer/Drink Mixer/CategoryViewController.h
@@ -0,0 +1,18 @@
+//
+//  CategoryViewController.h
+//  Drink Mixer
+//
+//  Created by Lucy Guo on 5/5/14.
+//  Copyright (c) 2014 Angela Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIButton *refreshersButton;
+@property (strong, nonatomic) IBOutlet UIButton *coffeeButton;
+@property (strong, nonatomic) IBOutlet UIButton *shakesButton;
+@property (strong, nonatomic) IBOutlet UIButton *juiceButton;
+
+@end
--- Drink Mixer/Drink Mixer/CategoryViewController.m
+++ Drink Mixer/Drink Mixer/CategoryViewController.m
@@ -1,32 +1,70 @@
 //
-//  HomeViewController.m
+//  CategoryViewController.m
 //  Drink Mixer
 //
-//  Created by Lucy Guo on 4/22/14.
+//  Created by Lucy Guo on 5/5/14.
 //  Copyright (c) 2014 Angela Zhang. All rights reserved.
 //
 
-#import "HomeViewController.h"
-//#import "CategoriesViewController.h"
 #import "CategoryViewController.h"
 #import "ShareViewController.h"
 
-@interface HomeViewController ()
+@interface CategoryViewController ()
 
 @end
 
-@implementation HomeViewController
+@implementation CategoryViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
+        // Custom initialization
         [self initializeNavBar];
-        
-        [self addBackgroundImage];
+        [self addSideImage];
+        [self addCatButtons];
+        self.view.backgroundColor = [UIColor whiteColor];
 
     }
     return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)addSideImage
+{
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 10, 70.5, 485)];
+    imgView.image = [UIImage imageNamed:@"sideimages.png"];
+    [self.view addSubview:imgView];
+}
+
+- (void)addCatButtons
+{
+    
+    //cocktails button
+    self.refreshersButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.refreshersButton setTitle:@"Show View" forState:UIControlStateNormal];
+    
+    self.refreshersButton.frame = CGRectMake(80, 15, 219.5, 20.5);
+    [self.refreshersButton addTarget:self action:@selector(buttonTouched:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIImage *btnImage = [UIImage imageNamed:@"refreshers.png"];
+    [self.refreshersButton setImage:btnImage forState:UIControlStateNormal];
+    self.refreshersButton.contentMode = UIViewContentModeScaleToFill;
+    
+    [self.view addSubview:self.refreshersButton];
+    
+    
 }
 
 - (void)initializeNavBar
@@ -45,21 +83,38 @@
     lbb.tintColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:.5];
     self.navigationItem.leftBarButtonItem = lbb;
     
+    
     // Logo in the center of navigation bar
     UIView *logoView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 44)];
     UIImageView *titleImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"navlogo.png"]];
     titleImageView.frame = CGRectMake(43, 8, titleImageView.frame.size.width/2, titleImageView.frame.size.height/2);
     [logoView addSubview:titleImageView];
+    
+    UIButton *homeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    homeButton.frame = CGRectMake(0, 0, 200, 44);
+    [homeButton addTarget:self
+                   action:@selector(launchHome)
+         forControlEvents:UIControlEventTouchUpInside];
+    [logoView addSubview:homeButton];
+    
+    titleImageView.alpha = .5;
+    
     self.navigationItem.titleView = logoView;
     
     // Right bar button item to launch the categories selection screen.
+    
     UIBarButtonItem *rbb = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"tinycup.png"]
                                                             style:UIBarButtonItemStylePlain
                                                            target:self
                                                            action:@selector(launchCategories)];
     
-    rbb.tintColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:.5];
+    rbb.tintColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
     self.navigationItem.rightBarButtonItem = rbb;
+}
+
+- (void)launchHome
+{
+    [self.navigationController popToRootViewControllerAnimated:NO];
 }
 
 - (void)launchShare
@@ -70,37 +125,9 @@
 
 - (void)launchCategories
 {
-    CategoryViewController *cvc = [[CategoryViewController alloc] init];
-    [self.navigationController pushViewController:cvc animated:NO];
+    // Already in the Categories view; this doesn't do anything.
 }
 
-- (void)viewDidLoad
-{
-    NSLog(@"HOME");
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)addBackgroundImage
-{
-//    UIGraphicsBeginImageContext(self.view.frame.size);
-//    [[UIImage imageNamed:@"home-bg.png"] drawInRect:self.view.bounds];
-//    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-//    UIGraphicsEndImageContext();
-//
-    
-    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 326.4, 120.36)];
-    imgView.image = [UIImage imageNamed:@"profile.png"];
-    [self.view addSubview:imgView];
-
-//    self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-}
 
 /*
 #pragma mark - Navigation
